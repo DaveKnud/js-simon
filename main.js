95f7041998b6816c9d230433ecdd7ca63c2a28cb
DaveKnud <daveknud@outlook.com>
2023-05-11 17:45:04 +0200

    Inserendo il prompt per risposte numeri

--- main.js
+++ main.js
@@ -7,15 +7,29 @@
 //* Dividete in piccoli problemi la consegna.
 //Individuate gli elementi di cui avete bisogno per realizzare il programma.
 
-//Const 
+//Container 
 const container = document.getElementById("container");
 console.log(container);
+
+//Genera numeri random da ricordare
 const numeriDaRicordare = generaOrdineNumeriRandom(1, 5);
 container.append(numeriDaRicordare);
 
+//Chiedere all'utente di ricordarsi i numeri precenti
+
+function rispostaUtente (){
+    let numeriRicordatiUser = prompt ("Inserisci qui' i numeri");
+    console.log(numeriRicordatiUser);
+    document.getElementById("container").innerHTML += numeriRicordatiUser;
+}
+
+setTimeout(rispostaUtente,3000);
 
 
 
+
+//FUNZIONI
+//Timing Function
 
 
 //create order random number
@@ -24,7 +38,7 @@ function generaOrdineNumeriRandom(min, max) {
   
     while(numeriRandom.length < max) {
           //Creare numero casuale nel min e max
-        const nuovoNum = generaNumeroRandom(min,max);
+        const nuovoNum = generaNumeroRandom(1,100);
         if(!numeriRandom.includes(nuovoNum)) {
             numeriRandom.push(nuovoNum);
         }
